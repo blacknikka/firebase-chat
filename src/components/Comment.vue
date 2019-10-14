cea3af873b006a74caec92a8bba08afb47a6769f
Yasuhiro <yasuhiro1026a@hotmail.com>
2019-10-14 20:22:29 +0900

    modify componetn

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-    This is Chat comment.
+  <div class="comment-container">
+    <div class="comment-content">
+      {{comment}}
+    </div>
+    <div class="author-name">
+      {{author}}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    comment: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    },
+  }
 };
 </script>
+
+<style scoped>
+.comment-container {
+  width: 100%;
+  background-color: lavender;
+}
+</style>
